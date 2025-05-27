02f05672286d2de5f26b85bf833002b48f84db9b
Andrew Udvare <audvare@gmail.com>
2025-05-27 14:02:58 -0400

    gentoo: cover the rest

--- tests/test_gentoo.py
+++ tests/test_gentoo.py
@@ -27,17 +27,16 @@ def test_clean_old_kernels_and_modules_success(mock_path: Mock, mock_rmtree: Moc
     mock_loc.exists.return_value = True
     mock_loc.is_symlink.return_value = True
     mock_loc.readlink.return_value = 'linux-5.10.0'
-
     mock_module_dir = mocker.MagicMock()
     mock_module_dir.is_dir.return_value = True
     mock_module_dir.__str__.return_value = 'module-1'
-
     mock_kernel_dir = mocker.MagicMock()
     mock_kernel_dir.is_dir.return_value = True
     mock_kernel_dir.__str__.return_value = 'linux-5.9.0'
-
-    mock_path.return_value.glob.side_effect = [[mock_module_dir], [mock_kernel_dir]]
-
+    other_non_dir = mocker.MagicMock()
+    other_non_dir.is_dir.return_value = False
+    mock_path.return_value.glob.side_effect = [[mock_module_dir, other_non_dir],
+                                               [mock_kernel_dir, other_non_dir]]
     result = list(clean_old_kernels_and_modules())
     assert result == ['module-1', 'linux-5.9.0']
     mock_rmtree.assert_any_call(mock_module_dir)
@@ -48,7 +47,6 @@ def test_clean_old_kernels_and_modules_no_symlink(mock_path: Mock) -> None:
     mock_loc = mock_path.return_value.__truediv__.return_value
     mock_loc.exists.return_value = True
     mock_loc.is_symlink.return_value = False
-
     with pytest.raises(InvalidActiveKernelSourcePath):
         list(clean_old_kernels_and_modules())
 
@@ -56,6 +54,5 @@ def test_clean_old_kernels_and_modules_no_symlink(mock_path: Mock) -> None:
 def test_clean_old_kernels_and_modules_file_not_found(mock_path: Mock) -> None:
     mock_loc = mock_path.return_value.__truediv__.return_value
     mock_loc.exists.return_value = False
-
     with pytest.raises(FileNotFoundError):
         list(clean_old_kernels_and_modules())
