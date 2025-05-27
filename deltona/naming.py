ec4f6809d4889c385552aeff8ebd9207890dbac7
Andrew Udvare <audvare@gmail.com>
2025-05-27 06:49:31 -0400

    naming: tests

--- deltona/naming.py
+++ deltona/naming.py
@@ -6,14 +6,13 @@ from typing import TYPE_CHECKING
 import re
 
 from .string import fix_apostrophes, is_roman_numeral
-from .typing import assert_not_none
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
-# non-strict, not including words like below, forms of to be, forms of
-#   you/he/etc, or words like 'call'
-STOP_WORDS = (
+# Non-strict, not including words like below, forms of to be, forms of you/he/etc, or words like
+# 'call'.
+STOP_WORDS = {
     'a',
     'an',
     'and',
@@ -50,20 +49,22 @@ STOP_WORDS = (
     'vs',
     'with',
     'within',
-    'without')
+    'without'
+}
 """Common stop words for English."""
 ENGLISH_ORDINAL_RE = r'(\d+)(st|nd|rd|th)'
 """Regular expression for English ordinals."""
-ENGLISH_ABBREV = ('feat', 'mr', 'mrs', 'ms', 'vs')
+ENGLISH_ABBREV = {'feat', 'mr', 'mrs', 'ms', 'vs'}
 """English abbreviations for period removal."""
-JAPANESE_PARTICLES = ('de', 'e', 'ga', 'ha', 'ka', 'kana', 'ne', 'ni', 'no', 'to', 'wa', 'wo')
+JAPANESE_PARTICLES = {'de', 'e', 'ga', 'ha', 'ka', 'kana', 'ne', 'ni', 'no', 'to', 'wa', 'wo'}
 """Only really common ones."""
-CHINESE_PARTICLES = ('de', 'ge', 'he', 'le', 'ma')
+CHINESE_PARTICLES = {'de', 'ge', 'he', 'le', 'ma'}
 """Only really common ones."""
-ARABIC_STOPS = ('al', 'ala', 'alayhi', 'alayka', 'alayya', 'an', 'anhu', 'anka', 'anni', 'bi',
-                'biha', 'bihi', 'bika', 'fi', 'fihi', 'fika', 'fiya', 'ila', 'ilayhi', 'ilayka',
-                'ilayya', 'lahu', 'laka', 'li', 'maa', 'maahu', 'maaka', 'mai', 'min', 'minhu',
-                'minka', 'minni', 'wa')
+ARABIC_STOPS = {
+    'al', 'ala', 'alayhi', 'alayka', 'alayya', 'an', 'anhu', 'anka', 'anni', 'bi', 'biha', 'bihi',
+    'bika', 'fi', 'fihi', 'fika', 'fiya', 'ila', 'ilayhi', 'ilayka', 'ilayya', 'lahu', 'laka', 'li',
+    'maa', 'maahu', 'maaka', 'mai', 'min', 'minhu', 'minka', 'minni', 'wa'
+}
 """Not a complete set."""
 NAMES_TO_FIX = {
     "mcdonald's": "McDonald's",
@@ -152,8 +153,8 @@ def adjust_title(words: str,
     """
     original_words = words.strip().split()
     word_list = words.strip().title().split()
-    name = _get_name(word_list[0], names=names)
-    if name is not None:
+    name = _get_name(word_list[0], names=names) if not disable_names else None
+    if name:
         title = [name]
     elif word_list[0].upper() == original_words[0]:
         title = [fix_apostrophes(original_words[0])]
@@ -163,38 +164,34 @@ def adjust_title(words: str,
         title = [title[0].upper()]
     last_index = len(original_words) - 1
     # MIX is a roman numeral but is more typically used in a sequence like 'Extended Mix', so do not
-    # capitalise it
-    ignore_roman = ('mix',)
+    # capitalise it.
+    ignore_roman = {'mix'}
     for mode in modes:
         to_lower_case_array = MODE_MAP[mode]
         index = 1
         for word in word_list[1:]:
             new_word = word
-            if disable_names is False:
-                name = _get_name(new_word, names=names)
-                if name is not None:
-                    try:
-                        title[index] = name
-                    except IndexError:
-                        title.append(name)
-                    index += 1
-                    continue
-            # Detect an upper-case new_word not to change
-            if original_words[index] == new_word.upper() and not re.match(r'[^\w]', new_word):
-                # Detect I (not Roman numeral necessarily)
-                if mode == Mode.English and new_word == 'I':
-                    pass
-                # ???
-                elif index == last_index and re.match(r'[A-Z0-9]+', original_words[index]):
-                    title.append(original_words[index])
-                    continue
+            if not disable_names and (name := _get_name(new_word, names=names)):
+                try:
+                    title[index] = name
+                except IndexError:
+                    title.append(name)
+                index += 1
+                continue
+            # Detect an upper-case new_word not to change.
+            # First detect I and ignore it, then detect uppercase letters and numbers.
+            if ((original_words[index] == new_word.upper() and not re.match(r'[^\w]', new_word))
+                    and (not (mode == Mode.English and new_word == 'I') and
+                         (index == last_index and re.match(r'[A-Z0-9]+', original_words[index])))):
+                title.append(original_words[index])
+                continue
             begin = end = ''
-            if re.match(r'^\W', new_word):
-                begin = assert_not_none(re.match(r'^(\W+)', new_word)).groups()[0]
-                new_word = new_word[1:]
-            if re.match(r'\w+\W$', new_word):
-                end = assert_not_none(re.match(r'\w+(\W+)$', new_word)).groups()[0]
-                new_word = new_word[0:-1]
+            if begin_match := re.match(r'^(\W+)', new_word):
+                begin = begin_match.group(0)
+                new_word = new_word[len(begin):]
+            if end_match := re.search(r'(\W+)$', new_word):
+                end = end_match.group(0)
+                new_word = new_word[0:-len(end)]
             if new_word.lower() in to_lower_case_array:
                 new_word = word_list[index] = new_word.lower()
             new_word = fix_apostrophes(new_word)
